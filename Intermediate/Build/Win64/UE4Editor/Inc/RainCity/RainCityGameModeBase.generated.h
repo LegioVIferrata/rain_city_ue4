6b36bd90a97b6a28879e40944e094cf2d414d1f6
Michał <LegioVIferrata@users.noreply.github.com>
2020-01-30 09:02:44 +0300

    With sound
    Жуй звезда Красная Бурда

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/RainCity/RainCityGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef RAINCITY_RainCityGameModeBase_generated_h
+#error "RainCityGameModeBase.generated.h already included, missing '#pragma once' in RainCityGameModeBase.h"
+#endif
+#define RAINCITY_RainCityGameModeBase_generated_h
+
+#define RainCity_Source_RainCity_RainCityGameModeBase_h_15_RPC_WRAPPERS
+#define RainCity_Source_RainCity_RainCityGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define RainCity_Source_RainCity_RainCityGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesARainCityGameModeBase(); \
+	friend RAINCITY_API class UClass* Z_Construct_UClass_ARainCityGameModeBase(); \
+public: \
+	DECLARE_CLASS(ARainCityGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/RainCity"), NO_API) \
+	DECLARE_SERIALIZER(ARainCityGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define RainCity_Source_RainCity_RainCityGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesARainCityGameModeBase(); \
+	friend RAINCITY_API class UClass* Z_Construct_UClass_ARainCityGameModeBase(); \
+public: \
+	DECLARE_CLASS(ARainCityGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/RainCity"), NO_API) \
+	DECLARE_SERIALIZER(ARainCityGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define RainCity_Source_RainCity_RainCityGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ARainCityGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ARainCityGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ARainCityGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ARainCityGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ARainCityGameModeBase(ARainCityGameModeBase&&); \
+	NO_API ARainCityGameModeBase(const ARainCityGameModeBase&); \
+public:
+
+
+#define RainCity_Source_RainCity_RainCityGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ARainCityGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ARainCityGameModeBase(ARainCityGameModeBase&&); \
+	NO_API ARainCityGameModeBase(const ARainCityGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ARainCityGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ARainCityGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ARainCityGameModeBase)
+
+
+#define RainCity_Source_RainCity_RainCityGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define RainCity_Source_RainCity_RainCityGameModeBase_h_12_PROLOG
+#define RainCity_Source_RainCity_RainCityGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RainCity_Source_RainCity_RainCityGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	RainCity_Source_RainCity_RainCityGameModeBase_h_15_RPC_WRAPPERS \
+	RainCity_Source_RainCity_RainCityGameModeBase_h_15_INCLASS \
+	RainCity_Source_RainCity_RainCityGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define RainCity_Source_RainCity_RainCityGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RainCity_Source_RainCity_RainCityGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	RainCity_Source_RainCity_RainCityGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	RainCity_Source_RainCity_RainCityGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	RainCity_Source_RainCity_RainCityGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID RainCity_Source_RainCity_RainCityGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Source/RainCity/RainCityGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RainCityGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAINCITY_API ARainCityGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
